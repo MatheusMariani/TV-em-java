9241d62b0ca0da29ef952912109ef84aa17b7280
MatheusMariani <48685739+MatheusMariani@users.noreply.github.com>
2019-04-04 10:52:47 -0300

    Create main.java

---
+++ main.java
@@ -0,0 +1,12 @@
+package television;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+            Scanner scan = new Scanner(System.in);
+            TV r = new TV();
+            r.menu();
+        }
+    
+}
